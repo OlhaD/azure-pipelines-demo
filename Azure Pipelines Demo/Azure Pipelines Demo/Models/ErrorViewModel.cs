dcc1ea5e09a37535cb365499f4f7f501256dfaec
Olha <olgadorogko@gmail.com>
2020-11-02 15:06:14 -0500

    Create empty demo MVC application (.NET Core)

---
+++ Azure Pipelines Demo/Azure Pipelines Demo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Azure_Pipelines_Demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
